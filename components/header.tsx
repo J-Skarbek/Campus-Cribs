2d1a7b1caed4287b184d89abcd75ee1c875c6912
J-Skarbek <john@snarkmediaagency.com>
2024-06-06 14:09:53 -0500

    Work on the header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,22 @@
 export default function Header() {
 
   return (
-    <header className="flex">
-      <div className="logo-container">
-        <img src="header-logo.png" alt='Campus Cribs Logo'></img>
+    <header className="flex bg-blue-50 justify-center align-center py-4 px-8">
+      <div className="flex align-center justify-between w-full max-w-screen-2xl">
+        <div className="logo-container">
+          <img src="header-logo.png" alt='Campus Cribs Logo'></img>
+        </div>
+        <div className="nav-menu-container flex items-center">
+          <nav>
+            <ul className="flex gap-4">
+              <li>Home</li>
+              <li>About</li>
+              <li>Listings</li>
+              <li>Whatever</li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <div className="nav-menu-container">
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Listings</li>
-            <li>Whatever</li>
-          </ul>
-        </nav>
-      </div>
-
     </header>
   )
 }
